e67efd09ba40c64f40f1a8cbc6e0e8d8eeaf1237
Adam_V <78228973+AdamosV@users.noreply.github.com>
2023-02-03 18:09:40 +0100

    Update capture_image.py

--- modules/capture_image.py
+++ modules/capture_image.py
@@ -1,9 +1,14 @@
 from picamera import PiCamera
+import os
 
 def capture_image(base_folder):
+    """
+    Captures images
+    """
+    image = os.path.join(base_folder, "images/last_image.jpg")
     camera = PiCamera()
     camera.resolution = (1296, 972)
     # Capture the image
-    camera.capture(base_folder + "/images/last_image.jpg")
+    camera.capture(image)
     
     return("images/last_image.jpg")
