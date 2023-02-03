63bfcc1da3c079fa827173647ecd92aa528add57
Adamus9 <76060398+Adamus9@users.noreply.github.com>
2023-02-03 11:54:21 +0100

    Add files via upload

---
+++ gps.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+def convert(angle):
+    """
+    překonvertuje úhel na .exif formát
+    """
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return sign < 0, exif_angle
+
+def capture(camera, image):
+    """vyfotí obráyek s polohou."""
+    point = ISS.coordinates()
+
+    # Convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(point.latitude)
+    west, exif_longitude = convert(point.longitude)
+
+    # Set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # Capture the image
+    camera.capture(image)
+
+cam = PiCamera()
+cam.resolution = (1296,972)
+
+base_folder = Path(__file__).parent.resolve()
+capture(cam, f"{base_folder}/gps1.jpg")
