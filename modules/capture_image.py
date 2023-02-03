7c64c38857c8bc363eef4562fa6deea5e8c9ab33
Adam_V <78228973+AdamosV@users.noreply.github.com>
2023-02-03 16:26:36 +0100

    Add files via upload

---
+++ modules/capture_image.py
@@ -0,0 +1,9 @@
+from picamera import PiCamera
+
+def capture_image(base_folder):
+    camera = PiCamera()
+    camera.resolution = (1296, 972)
+    # Capture the image
+    camera.capture(base_folder/images/"last_image.jpg")
+    
+    return(images/"last_image.jpg")
